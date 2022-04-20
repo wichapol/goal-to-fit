3e21a552f329a5e843569c4307cdff711e66a521
wichapol jaimaung <wichapol.44424@gmail.com>
2022-04-20 23:44:00 +0700

    edit URL 4000

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const client = axios.create({
-  baseURL: "https://backendgoaltofit.vercel.app",
+  baseURL: "http://localhost:4000",
 });
 
 export const getRecords = async () => {
